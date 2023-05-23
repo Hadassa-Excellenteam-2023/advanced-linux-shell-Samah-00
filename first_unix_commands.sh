23f633f8a551723089933cf3a8612a49b7b1b584
samahrajabi <samahrajabi.sr@gmail.com>
2023-05-23 17:52:53 +0300

    ex1 done

---
+++ first_unix_commands.sh
@@ -0,0 +1,13 @@
+echo "Samah" > myname.txt
+touch myname.txt
+echo "Samah" >> myname.txt
+mkdir new
+cp myname.txt new/
+mv new/myname.txt new/samah.txt
+gedit new/samah.txt
+cp new/samah.txt new/newFile.txt
+cd new
+ls
+cd ..
+cd ~
+rm -rf new
